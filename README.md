fa6cf79c3f9852328bdeb85f4bbd1237c2fc2844
asharaloran96 <asharoran96@gmail.com>
2020-02-09 22:05:50 +0200

    do the lab

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# lab-16
+# lab-16 - Event Driven ApplicaTion 
+
+## Auther : As-har Aloran
+
+## Links and Resources
+* (pull request) : [] 
+* (ci/cd) : [https://github.com/asharoran-401-advanced-javascript/lab-16/actions] 
+
+## How to initialize/run your application (where applicable)
+- npm start
+- nodemon
+
+## Tests
+- npm test
+- npm run test
+## UML
+
+* whiteBoard : ![alt text](assets/lab-16.jpg)
---
+++ __tests__/app.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undefined */
+// eslint-disable-next-line strict
+'use strict';
+
+const fs = require('fs');
+const events = require('../lib/events.js');
+const log = require('../lib/logger.js');
+
+
+describe(' File system' , () =>{
+  it(' Error form bad file', done => {
+    fs.readFile('./a/lib/file.js', (err, data) => {
+      console.log('----error----' , err);
+      expect(err).toBeDefined();
+      console.log('----data----' , data);
+      expect(data).toBe(undefined);
+      done();
+    });
+  });
+
+  it('Create a log without Payload' , () =>{
+    log('saveFile');
+    console.log('-----' , log);
+    expect(log('saveFile')).toBe(undefined);
+  });
+});
